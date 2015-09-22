7c37599b3f989494ac0265efdfa77da06d4cba56
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-22 14:00:49 +0200

    Ready for distribution

--- dist.sh
+++ dist.sh
@@ -6,12 +6,12 @@ shift
 
 sudo chmod -R a+rw $WORK_DIR
 
-docker rm r-build 2> /dev/null | true
+docker rm r-dist 2> /dev/null | true
 
 # Bind mount your data
 # assuming that current folder contains the data
 docker run -v $WORK_DIR:/home/docker/data:rw \
-    --rm --name r-build \
-    registry.federation.mip.hbp/hbp_tools/r-interactive build-package 2>&1 | sed -e "s|/home/docker/data|$WORK_DIR|g"
+    --rm --name r-dist \
+    registry.federation.mip.hbp/hbp_tools/r-interactive dist-package 2>&1 | sed -e "s|/home/docker/data|$WORK_DIR|g"
 
 sudo chown -R $USER:$USER $WORK_DIR
--- tests/testthat/testNode.R
+++ tests/testthat/testNode.R
@@ -1,27 +1,29 @@
 context("Node")
 
-library(hbpboxstat)
+library(hbpboxstats)
 
-test_that("Linear regression is correct at the node level", {
+test_that("Box statistics are correct at the node level", {
 
     set.seed(100)
 
 	N <- 20
 
-    y <- matrix(rnorm(M,mean=2,sd=1), M, 1)
+    y <- matrix(rnorm(N,mean=2,sd=1), N, 1)
     
     stats <- BoxStats_node(y)
-    print(xest)
+    print(stats)
 
     min <- stats[["min"]]
     q1 <- stats[["q1"]]
     median <- stats[["median"]]
     q3 <- stats[["q3"]]
     max <- stats[["max"]]
+
+    str(min)
     
-    expect_equal(min, 0.71517135)
-    expect_equal(q1, 2.60860245)
-    expect_equal(median, 1.06780877)
-    expect_equal(q3, 0.0363542484)
-    expect_equal(max, -0.0033319972)
+    expect_equal(min, 1.086186, tolerance = 1e-6)
+    expect_equal(q1, 1.63289,  tolerance = 1e-6)
+    expect_equal(median, 2.09308,  tolerance = 1e-6)
+    expect_equal(q3, 2.366687,  tolerance = 1e-6)
+    expect_equal(max, 4.310297,  tolerance = 1e-6)
 })
