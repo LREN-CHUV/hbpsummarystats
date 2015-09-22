e76d6e5d0e4f1274beed8a33b9cd9a17cfb8a29c
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-22 12:30:47 +0200

    Update structure

--- R/node.R
+++ R/node.R
@@ -10,22 +10,20 @@
 #' @keywords boxplot
 #' @export
 BoxStats_node <- function(y) {
-  
-  # Lester Melie-Garcia
-  # LREN, CHUV. 
-  # Lausanne, September 11th, 2015
-  
-  ymean <- mean(y);
-  ymedian <- median(y);
-  ystd <- sd(y);
-  ymin <-  min(y);
-  ymax <- max(y);
-  yq <- quantile(y);
-  q1 <- yq[[2]]; 
-  q3 <- yq[[4]];
-  
-  rout <- list(ymin, q1, ymedian, q3, ymax)
-  names(rout) <- c("min", "q1", "median", "q3", "max")
-  
-  return(rout)
-}
+    
+    # Lester Melie-Garcia LREN, CHUV.  Lausanne, September 11th, 2015
+    
+    ymean <- mean(y)
+    ymedian <- median(y)
+    ystd <- sd(y)
+    ymin <- min(y)
+    ymax <- max(y)
+    yq <- quantile(y)
+    q1 <- yq[[2]]
+    q3 <- yq[[4]]
+    
+    rout <- list(ymin, q1, ymedian, q3, ymax)
+    names(rout) <- c("min", "q1", "median", "q3", "max")
+    
+    return(rout)
+} 
