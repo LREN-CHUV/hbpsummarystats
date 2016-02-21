27cc627533e888044f6465b646f61c93c42f93f0
Ludovic Claude <ludovic.claude@laposte.net>
2016-02-21 22:27:12 +0000

    Update dev scripts

--- dev.sh
+++ dev.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
 WORK_DIR="$(pwd)"
-shift
+
+if groups $USER | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
+else
+  DOCKER="sudo docker"
+fi
 
 sudo chmod -R a+rw $WORK_DIR
 
-docker rm r-dev 2> /dev/null | true
+$DOCKER rm r-dev 2> /dev/null | true
 
 echo "Cheat sheet - run the following commands:"
 echo
+echo "setwd('/src')"
 echo "formatR::tidy_dir(\"R\")"
-echo "  Format your source code"
+echo "  # Format your source code"
 echo
+echo "setwd('/src')"
 echo "lintr::lint_package()"
-echo "  Checks the style of the source code"
+echo "  # Checks the style of the source code"
 echo
+echo "setwd('/src')"
 echo "devtools::load_all()"
-echo "  Load the code in the current project"
+echo "  # Load the code in the current project"
 echo
+echo "setwd('/src')"
 echo "devtools::document()"
-echo "  Generates the documentation"
+echo "  # Generates the documentation"
 echo
+echo "setwd('/src')"
 echo "devtools::use_testthat()"
-echo "  Setup the package to use testthat"
+echo "  # Setup the package to use testthat"
 echo
 echo "-----------------------------------------"
 
 # Bind mount your data
 # assuming that current folder contains the data
-docker run -v $WORK_DIR:/home/docker/data:rw \
+$DOCKER run -v $WORK_DIR:/home/docker/data:rw \
+    -v $WORK_DIR/:/src/ \
+    -v $WORK_DIR/tests:/src/tests/ \
     -i -t --rm --name r-dev \
-    registry.federation.mip.hbp/mip_tools/r-interactive R
+    hbpmip/r-interactive R
 
 sudo chown -R $USER:$USER $WORK_DIR
--- test.sh
+++ test.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 WORK_DIR="$(pwd)"
-shift
+
+if groups $USER | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
+else
+  DOCKER="sudo docker"
+fi
 
 sudo chmod -R a+rw $WORK_DIR
 
-docker rm r-test 2> /dev/null | true
+$DOCKER rm r-test 2> /dev/null | true
 
 # Bind mount your data
 # assuming that current folder contains the data
-docker run -v $WORK_DIR:/home/docker/data:rw \
+$DOCKER run -v $WORK_DIR:/home/docker/data:rw \
     --rm --name r-test \
-    registry.federation.mip.hbp/mip_tools/r-interactive check-package 2>&1 | sed -e "s|/home/docker/data|$WORK_DIR|g"
+    hbpmip/r-interactive check-package 2>&1 | sed -e "s|/home/docker/data|$WORK_DIR|g"
 
 sudo chown -R $USER:$USER $WORK_DIR
